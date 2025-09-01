68565ae7132b84483c71d444e4d72db25e2eaac8
scrumblous <scrumblous@proton.me>
2025-09-01 22:08:10 +0300

    fetch

---
+++ routes/fetch.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const { spawn } = require("node:child_process")
+
+router.get("/fetch", (req, res) => {
+    let command = spawn("fastfetch", ["-l", "none"])
+    command.stdout.on('data', (data) => {
+        res.send(data)
+    })
+})
